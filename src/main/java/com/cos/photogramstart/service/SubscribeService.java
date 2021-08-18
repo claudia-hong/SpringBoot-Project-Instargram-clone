5b46619ee54d58bd046ed303ef9d7e87132f1fae
Claudia <claudia_h@naver.com>
2021-08-19 04:08:16 +0900

    프로필 페이지 구독버튼 기능 및 구독정보 구현완료

--- src/main/java/com/cos/photogramstart/service/SubscribeService.java
+++ src/main/java/com/cos/photogramstart/service/SubscribeService.java
@@ -2,6 +2,10 @@ package com.cos.photogramstart.service;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import org.qlrm.mapper.JpaResultMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,11 +20,27 @@ import lombok.RequiredArgsConstructor;
 public class SubscribeService {
 	
 	private final SubscribeRepository subscribeRepository;
+	private final EntityManager em;
 	
 	@Transactional(readOnly = true)
 	public List<SubscribeDto> subscribeList(int principalId, int pageUserId){
 		
-		return null;
+		StringBuffer sb = new StringBuffer();
+		sb.append("SELECT u.id, u.username, u.profileImageUrl, ");
+		sb.append("if ((SELECT 1 FROM subscribe WHERE fromUserId =? AND toUserId=u.id),1,0) subscribeState, ");
+		sb.append("if((?=u.id),1,0) equalUserState ");
+		sb.append("FROM user u INNER JOIN subscribe s ");
+		sb.append("ON u.id = s.toUserId ");
+		sb.append("WHERE s.fromUserId =?");
+		
+		Query query=em.createNativeQuery(sb.toString())
+				.setParameter(1, principalId)
+				.setParameter(2, principalId)
+				.setParameter(3, pageUserId);
+		
+		JpaResultMapper result = new JpaResultMapper();
+		List<SubscribeDto> subscribeDtos = result.list(query, SubscribeDto.class);
+		return subscribeDtos;
 	}
 	
 	
